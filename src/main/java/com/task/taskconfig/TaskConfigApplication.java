060774ad30c5ea27026950601f80e7b44ad904bc
dinesh1794 <dineshnarayanaa@gmail.com>
2019-11-10 21:45:24 +0530

    My develop Commit

--- src/main/java/com/task/taskconfig/TaskConfigApplication.java
+++ src/main/java/com/task/taskconfig/TaskConfigApplication.java
@@ -3,6 +3,7 @@ package com.task.taskconfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -10,6 +11,11 @@ public class TaskConfigApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskConfigApplication.class, args);
+	}
+
+	@Bean
+	public Exception getException() {
+		return new Exception();
 	}
 
 }
